2312a7f0a26d4f28f48c410769ad876df779350d
MouaYing <jonandaprilpimentel@gmail.com>
2014-04-03 21:24:43 -0600

    RBDGameStore documentation

--- RDBGameStore.js
+++ RDBGameStore.js
@@ -1,11 +1,37 @@
 var ServerModelClass = require('../../models/server_model.js');
 
+	/**
+    The RDBGameStore class is returned by RDBPersistenceFactory and follows the IGameStore convention (interface)  
+    <pre>
+
+    Domain: None
+      
+    Invariants:
+        INVARIANT: None
+    
+    Constructor Specification:
+    	PRE: None
+    	POST: None
+    </pre>
+
+    @class RDBGameStore
+    @constructor
+    
+	*/
 var RDBGameStore = (function() {
 
 	function RDBGameStore(){
 	}
 
-	//ID is optional. If specified, a checkpoint will be added instead of a new model
+	/**
+    Returns modelId, the index where the model is stored  
+    ID is optional. If specified, a checkpoint will be added instead of a new model
+
+    @method addModel
+    @param {ServerModel} model
+    @return {int} The index of the new model 
+    
+	*/
 	RDBGameStore.prototype.addModel = function(modelJson, id) {
 		this.startTransaction();
 
@@ -17,17 +43,41 @@ var RDBGameStore = (function() {
 		return modelId;
 	}
 
+	/**
+    Returns the game model specified by id
+
+    @method getModel
+    @param {int} id
+    @return {ServerModel} The game model specified
+    
+	*/
 	RDBGameStore.prototype.getModel = function(id) {
 		var modelJson = null; //modelsTable where modelId == id
 
        	return modelJson;
 	}
 
+	/**
+    Removes the model specified by id from the table in the RDB
+
+    @method removeModel
+    @param {int} id
+    @return void
+    
+	*/
 	RDBGameStore.prototype.removeModel = function(id) {
 		//DELETE FROM modelsTable
 		//WHERE modelId=id;
 	}
 
+	/**
+    Returns a game model from the last checkpoint
+
+    @method getCheckpoint
+    @param {int} id
+    @return {ServerModel} the game model of the last checkpoint
+    
+	*/
 	RDBGameStore.prototype.getCheckpoint = function(id) {
 		//Get latest checkpoint for given game id
 		var modelJson = null;
@@ -35,6 +85,13 @@ var RDBGameStore = (function() {
 		return modelJson;
 	}
 
+	/**
+    Loads game models into memory from the RDB 
+
+    @method initialize
+    @return {ServerModel[]} An array of game models
+    
+	*/
 	RDBGameStore.prototype.initialize = function() {
 		console.log("\nINITIALIZING DATA STORE");
 		var models = [];
@@ -52,10 +109,24 @@ var RDBGameStore = (function() {
 		return models;
 	}
 
+	/**
+    Starts a transaction between the game and the RDB
+
+    @method startTransaction
+    @return {void}
+    
+	*/
 	RDBGameStore.prototype.startTransaction = function() {
 		console.log("Start Transaction");
 	}
 
+	/**
+    Ends a transaction between the game and the RDB
+
+    @method endTransaction
+    @return {void}
+    
+	*/
 	RDBGameStore.prototype.endTransaction = function() {
 		console.log("End Transaction");
 	}
