920486eb615c1685cbdb54ab60be72a0dda4d4db
tddawson <tddawson@gmail.com>
2014-04-03 20:43:17 -0600

    Added YUI Documentation to the methods in RDBCommandStore and RDBUserStore classes

--- RDBCommandStore.js
+++ RDBCommandStore.js
@@ -1,9 +1,41 @@
 
 
+/**
+    The RDBCommandStore class provides access to the Commands portion of an RDB.
+    <pre>
+
+    Author: Team 10    
+
+	Domain:
+		connection: A handle to the relational database.
+        
+    Invariants:
+        INVARIANT: RDBCommandStore can always access or create a valid read/write connection with a relational Database.
+
+    Constructor Specification:
+        PRE: Parameters provide access to the database, either by providing credentials or a file location (for SQLite)
+        POST: RDBCommandStore is now connected to an RDB and is able to manipulate and run queries against it.
+    </pre>
+
+    @class RDBCommandStore
+    @constructor
+*/
 var RDBCommandStore = (function() {
 
 	function RDBCommandStore(){
 	}
+
+	/**
+	Adds a command to the provided game's command list.
+	<pre>
+		PRE: gameId is a valid game in the database.
+		POST: The command list for the specified game now is associated with this command.
+	</pre>
+	@method addCommand
+	@param {Number} gameId The game id to connect the command with in the database
+	@param {String} commandJson The JSON representation of the command to add.
+	@return {Number} Returns the id of the command after being added to the database
+	*/
 
 	RDBCommandStore.prototype.addCommand = function(gameId, commandJson){
 		this.startTransaction();
@@ -16,6 +48,17 @@ var RDBCommandStore = (function() {
 		return commandId;
 	}
 
+	/**
+	Gets and returns all the commands for the specified game.
+	<pre>
+		PRE: gameId is a valid game ID in the database
+		POST: The database has not been modified.
+		POST: An array of that game's commands is returned.
+	</pre>
+	@method getCommands
+	@param {Number} gameId Determines which game's commands should be returned from the database
+	@return {List<String>} Returns an array of JSON-encoded commands.
+	*/
 	RDBCommandStore.prototype.getCommands = function(gameId){
 	{
 		var commands = [];
@@ -24,10 +67,28 @@ var RDBCommandStore = (function() {
 		return commands;
 	}
 
+	/**
+	Starts a transaction with the database
+    <pre>
+        PRE: A valid connection is established with the DB.
+        POST: A transaction has been started.
+    </pre>
+    @method startTransaction
+    */
 	RDBCommandStore.prototype.startTransaction = function() {
 		console.log("Start Transaction");
 	}
 
+
+	/**
+	Ends the current transaction with the database.
+    <pre>
+        PRE: A transaction exists.
+        POST: All changes to the DB are either saved or rolled back
+        POST: No transaction remains
+    </pre>
+    @method startTransaction
+    */
 	RDBCommandStore.prototype.endTransaction = function() {
 		console.log("End Transaction");
 	}
--- RDBUserStore.js
+++ RDBUserStore.js
@@ -1,10 +1,43 @@
+/**
+    The RDBUserStore class provides access to the Users table of a Relational Database
+    <pre>
 
+    Author: Team 10    
 
+    Domain:
+		connection: A handle to the relational database.
+        
+    Invariants:
+        INVARIANT: RDBUserStore can always access or create a valid read/write connection with a relational Database.
+
+    Constructor Specification:
+        PRE: Parameters provide access to the database, either by providing credentials or a file location (for SQLite)
+        POST: RDBUserStore is now connected to an RDB and is able to manipulate and run queries against it.
+    </pre>
+
+    @class RDBUserStore
+    @constructor
+*/
 var RDBUserStore = (function() {
 
 	function RDBUserStore(){
 	}
 
+	/**
+	Adds a user to the database and returns the user's primary key.
+    <pre>
+        PRE: The userName doesn't exist already in the database
+		PRE: userName is a valid user name as defined in the specs
+        PRE: password is a valid password as defined in the specs.
+        POST: The Users table of the RDB contains exactly one more entry than before.
+        POST: The user has been added to the database with his or her corresponding password
+        POST: The user's primary key in the database is returned.
+    </pre>
+    @method addUser
+	@param {String} userName The user name of the user to be added to the database
+	@param {String} password The raw string value of the password.
+	@return {Number}  Return the pk of the user just added.
+    */
 	RDBUserStore.prototype.addUser = function(userName, password){
 		if(!this.canAddUser(userName))
 			throw Error("User " + userName + " already exists.");
@@ -19,10 +52,30 @@ var RDBUserStore = (function() {
 		return userId;
 	}
 
+	/**
+	Deletes a user from the database.
+    <pre>
+        PRE: The 'id' is found in the database
+        POST: The Users table of the RDB contains exactly one less entry than before.
+        POST: The user with 'id' no longer is in the database.
+    </pre>
+    @method deleteUser
+	@param {Number} id The primary key of the user to delete from the database
+    */
 	RDBUserStore.prototype.deleteUser = function(id){
 		console.log("Delete user");
 	}
 
+	/**
+	Returns the JSON representation of a user from the RDB with a given username
+    <pre>
+        PRE: 'name' is a valid user name in the database
+        POST: Returns exactly 1 result who matches the parameter
+    </pre>
+    @method getUserByName
+	@param {String} name The user name of the user you are retrieving from the RDB.
+    @return {String} Returns the JSON representation of that user.
+    */
 	RDBUserStore.prototype.getUserByName = function(name){
 		
 		var userJson = null; //usersTable where userName == name
@@ -30,10 +83,27 @@ var RDBUserStore = (function() {
 		return userJson;
 	}
 
+	/**
+	Starts a transaction with the database
+    <pre>
+        PRE: A valid connection is established with the DB.
+        POST: A transaction has been started.
+    </pre>
+    @method startTransaction
+    */
 	RDBUserStore.prototype.startTransaction = function() {
 		console.log("Start Transaction");
 	}
 
+	/**
+	Ends the current transaction with the database.
+    <pre>
+        PRE: A transaction exists.
+        POST: All changes to the DB are either saved or rolled back
+        POST: No transaction remains
+    </pre>
+    @method endTransaction
+    */
 	RDBUserStore.prototype.endTransaction = function() {
 		console.log("End Transaction");
 	}
