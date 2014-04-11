1caeb8bd7bdcec8ae13584c791540ab25d37ec2a
Tyson Decker <tyson.decker@gmail.com>
2014-04-10 23:53:17 -0600

    Wrote tests for and implemented updateGame, removeGame, and initialize

--- test/test-game-store.js
+++ test/test-game-store.js
@@ -3,15 +3,16 @@ var RDBGameStore = require('../lib/RDBGameStore.js');
 var _ = require('underscore');
 
 var tableName = "Games";
+var checkpointTable = "Checkpoints";
 var database = "data/catan-test.sqlite";
 
 describe("RDB Game Store", function() {
 
 	it('Add a game', function(done) {
 
-		var gameStore = new RDBGameStore.RDBGameStore(database, tableName);
+		var gameStore = new RDBGameStore.RDBGameStore(database, tableName, checkpointTable);
 
-		gameStore.addGame([{id:0,title:"hello",model:{some:"stuff",goes:"here"}}], function(id){
+		gameStore.addGame({id:0,title:"hello",model:{some:"stuff",goes:"here"}}, function(id){
 			assert.notEqual(id, "undefined");
 			gameStore.removeGame(id);
 			gameStore.close();
@@ -22,7 +23,7 @@ describe("RDB Game Store", function() {
 
 	it('Get a game from the database', function(done) {
 
-		var gameStore = new RDBGameStore.RDBGameStore(database, tableName);
+		var gameStore = new RDBGameStore.RDBGameStore(database, tableName, checkpointTable);
 
 		gameStore.addGame({id:0,title:"Tyson's Game",model:{some:"stuff",goes:"here"}}, function(id){
 			gameStore.getGame(id, function(game){
@@ -40,9 +41,9 @@ describe("RDB Game Store", function() {
 
 	it('Remove a game from the database', function(done) {
 
-		var gameStore = new RDBGameStore.RDBGameStore(database, tableName);
+		var gameStore = new RDBGameStore.RDBGameStore(database, tableName, checkpointTable);
 
-		gameStore.addGame([{id:0,title:"hello",model:{some:"stuff",goes:"here"}}], function(id){
+		gameStore.addGame({id:0,title:"hello",model:{some:"stuff",goes:"here"}}, function(id){
 			gameStore.removeGame(id, function(){
 				gameStore.getGame(id, function(game){
 					assert.equal(game, null);
@@ -52,5 +53,89 @@ describe("RDB Game Store", function() {
 		});
 
 	});
+
+	it('Update a game in the database', function(done) {
+
+		var gameStore = new RDBGameStore.RDBGameStore(database, tableName, checkpointTable);
+
+		gameStore.addGame({id:0,title:"hello",model:{some:"stuff",goes:"here"}}, function(id){
+			gameStore.updateGame({id:1,title:"helloagain",model:{some:"stuff",goes:"here"}}, id, function(changes){
+
+				assert.equal(changes, 1);
+
+				gameStore.getGame(id, function(game){
+
+					assert.equal(game.id, 1);
+					assert.equal(game.title, "helloagain");
+					gameStore.removeGame(id);
+					done();
+
+				});
+
+			})
+		});
+
+	});
+
+	it('Add a checkpoint', function(done) {
+
+		var gameStore = new RDBGameStore.RDBGameStore(database, tableName, checkpointTable);
+
+		assert.ok(false, "Test not build yet");
+
+	});
+
+	it('Get a checkpoint', function(done) {
+
+		var gameStore = new RDBGameStore.RDBGameStore(database, tableName, checkpointTable);
+
+		assert.ok(false, "Test not build yet");
+
+	});
+
+	it('Initialize with no games', function(done) {
+
+		var gameStore = new RDBGameStore.RDBGameStore(database, tableName, checkpointTable);
+
+		// Test without any games in the database
+		gameStore.initialize(function(models){
+
+			assert.equal(models.length, 0);
+			done();
+		});
+
+	});
+
+	it('Initialize with games', function(done) {
+
+		var gameStore = new RDBGameStore.RDBGameStore(database, tableName, checkpointTable);
+
+
+		// Test with some games
+		gameStore.addGame({id:0,title:"Game1",model:{some:"stuff",goes:"here"}}, function(id){
+
+			var id1 = id;
+
+			gameStore.addGame({id:1,title:"Game2",model:{some:"stuff",goes:"here"}}, function(id){
+
+				var id2 = id;
+
+				gameStore.initialize(function(models){
+
+					assert.equal(models.length, 2);
+					assert.equal(models[0].title, "Game1");
+					assert.equal(models[1].title, "Game2");
+
+					gameStore.removeGame(id1, function(){
+						gameStore.removeGame(id2, function(){
+							done();
+						});
+					});
+
+				});
+			});
+		});
+	});
+
 
 });
